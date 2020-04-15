2a72890344ecd07fd715814439d5c144ba2b82bc
Artakhv <artak.hovhannisyan10@gmail.com>
2020-04-15 19:47:20 +0400

    add state && form

--- ReactAndRedux/CourseProject/src/components/courses/CoursesPage.js
+++ ReactAndRedux/CourseProject/src/components/courses/CoursesPage.js
@@ -1,8 +1,33 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
 class CoursesPage extends Component {
-    render(){
-        return <h2>Courses</h2>
+
+    state = {
+        course: {
+            title: ""
+        }
+    };
+
+    handleChange = (event) => {
+        const course = { ...this.state.course, title: event.target.value };
+        this.setState({ course });
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.course.title)
+    };
+
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h2>Courses</h2>
+                <h3>Add Course</h3>
+                <input type="text" onChange={this.handleChange} value={this.state.course.title} />
+                <input type="submit" value="Save" />
+            </form>
+        )
     }
 }
 
