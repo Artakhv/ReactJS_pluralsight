7c284f92ea9d9a8e8e6e84516532727bf0d335c4
Artakhv <artak.hovhannisyan10@gmail.com>
2020-04-19 13:11:04 +0400

    add new actions, reducers , logic

--- ReactAndRedux/CourseProject/src/components/courses/CoursesPage.js
+++ ReactAndRedux/CourseProject/src/components/courses/CoursesPage.js
@@ -1,43 +1,66 @@
-import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
-import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import CourseList from './CourseList';
+import * as authorActions from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+import CourseList from "./CourseList";
 
 class CoursesPage extends Component {
-    componentDidMount() {
-        this.props.actions.loadCourses().catch(error => {
-            alert("Loading courses error" + error)
-        });
+  componentDidMount() {
+      //// here I can use destructuring
+    if (this.props.courses.length === 0) {
+      this.props.actions.loadCourses().catch((error) => {
+        alert("Loading courses error" + error);
+      });
     }
+    if (this.props.authors.length === 0) {
+      this.props.actions.loadAuthors().catch((error) => {
+        alert("Loading authors error" + error);
+      });
+    }
+  }
 
-    render() {
-        return (
-            <Fragment>
-                <h2>Courses</h2>
-                <CourseList courses={this.props.courses} />
-            </Fragment>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <h2>Courses</h2>
+        <CourseList courses={this.props.courses} />
+      </Fragment>
+    );
+  }
 }
 
 CoursesPage.propTypes = {
-    courses: PropTypes.array.isRequired,
-    actions: PropTypes.object.isRequired
-}
+  authors: PropTypes.array.isRequired,
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
+};
 
 function mapStateToProps(state) {
-    return {
-        courses: state.courses
-    }
+  return {
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map((course) => {
+            return {
+              ...course,
+              authorName: state.authors.find((a) => a.id === course.authorId)
+                .name,
+            };
+          }),
+    authors: state.authors,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        // createCourse: course => dispatch(courseActions.createCourse(course))
-        actions: bindActionCreators(courseActions, dispatch)
-    }
+  return {
+    // createCourse: course => dispatch(courseActions.createCourse(course))
+    actions: {
+      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
+    },
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
