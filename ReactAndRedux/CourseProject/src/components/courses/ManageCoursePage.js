875d95aa55a0ce6ea203aef0cdfcb4287489b3b8
Artakhv <artak.hovhannisyan10@gmail.com>
2020-04-19 22:23:17 +0400

    2nd redux flow

--- ReactAndRedux/CourseProject/src/components/courses/ManageCoursePage.js
+++ ReactAndRedux/CourseProject/src/components/courses/ManageCoursePage.js
@@ -1,10 +1,15 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
 
 function ManageCoursePage(props) {
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({ ...props.errors });
+
   useEffect(() => {
     //// here I can use destructuring
     if (props.courses.length === 0) {
@@ -17,24 +22,50 @@ function ManageCoursePage(props) {
         alert("Loading authors error" + error);
       });
     }
-  },[]);
+  }, []);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setCourse((prevCourse) => {
+      // debugger;
+      return {
+        ...prevCourse,
+        [name]: name === "authorId" ? parseInt(value, 10) : value,
+      };
+    });
+  }
+
+  function handleSave(event) {
+    event.preventDefault();
+    console.log(course)
+    props.saveCourse(course);
+  }
 
   return (
     <Fragment>
-      <h2>Manage Course</h2>
+      <CourseForm
+        course={course}
+        errors={errors}
+        authors={props.authors}
+        onChange={handleChange}
+        onSave={handleSave}
+      />
     </Fragment>
   );
 }
 
 ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
   loadCourses: PropTypes.func.isRequired,
   loadAuthors: PropTypes.func.isRequired,
+  saveCourse: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
   return {
+    course: newCourse,
     courses: state.courses,
     authors: state.authors,
   };
@@ -44,6 +75,7 @@ function mapStateToProps(state) {
 const mapDispatchToProps = {
   loadCourses: courseActions.loadCourses,
   loadAuthors: authorActions.loadAuthors,
+  saveCourse: courseActions.saveCourse,
 };
 
 // bind way
